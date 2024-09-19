c2ff1c1bea550fa2368427c32bdacbd58ff46d54
Will Alicar <will@alicar.me>
2024-09-19 07:00:38 -0700

    setup for audio transcoding

--- app/main.cc
+++ app/main.cc
@@ -17,9 +17,9 @@ extern "C" {
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
 #include <libavutil/imgutils.h>
+#include <libavutil/log.h>
 #include <libavutil/rational.h>
 #include <libswscale/swscale.h>
-#include <libavutil/log.h>
 };
 
 const int DST_WIDTH = 1280;
@@ -47,13 +47,11 @@ MediaContext *decoder;
 char entry[512];
 bool is_callback_set = false;
 
-EM_JS(void, call_log, (const char *text), {
-  console.log(UTF8ToString(text));
-});
+EM_JS(void, call_log, (const char *text), { console.log(UTF8ToString(text)); });
 
-void custom_log_callback(void* ptr, int level, const char* fmt, va_list vargs) {
-    vsnprintf(entry, sizeof(entry), fmt, vargs);
-    call_log(entry);
+void custom_log_callback(void *ptr, int level, const char *fmt, va_list vargs) {
+  vsnprintf(entry, sizeof(entry), fmt, vargs);
+  call_log(entry);
 }
 
 int init(const char *filename) {
@@ -70,8 +68,10 @@ int init(const char *filename) {
     return -1;
   }
 
-  vid_idx = av_find_best_stream(decoder->avfc, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
-  aud_idx = av_find_best_stream(decoder->avfc, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
+  vid_idx =
+      av_find_best_stream(decoder->avfc, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
+  aud_idx =
+      av_find_best_stream(decoder->avfc, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
   decoder->video_avs = decoder->avfc->streams[vid_idx];
   decoder->audio_avs = decoder->avfc->streams[aud_idx];
 
@@ -86,7 +86,6 @@ int init(const char *filename) {
 
   decoder->video_avcc = avcodec_alloc_context3(decoder->video_avc);
   if (!decoder->video_avcc) {
-  if (!decoder->video_avcc) {
     printf("Could not allocate decoder context\n");
     return -1;
   }
@@ -97,60 +96,60 @@ int init(const char *filename) {
   }
 
   decoder->video_avcc->pkt_timebase = decoder->video_avs->time_base;
-  decoder->video_avcc->framerate = av_guess_frame_rate(NULL, decoder->video_avs, NULL);
+  decoder->video_avcc->framerate =
+      av_guess_frame_rate(NULL, decoder->video_avs, NULL);
 
   if (avcodec_open2(decoder->video_avcc, decoder->video_avc, NULL) < 0) {
     printf("Could not open decoder\n");
     return -1;
   }
 
-  // audio left off here.
+  // TODO: transcode audio
 
-
-  // setup output
+  // setup encoder
   if (avformat_alloc_output_context2(&encoder->avfc, NULL, NULL, "output.mp4") <
       0) {
     printf("Could not open output file to get format\n");
     return -1;
   }
 
-  encoder->avc = avcodec_find_encoder_by_name("libx264");
-  if (!encoder->avc) {
+  encoder->video_avc = avcodec_find_encoder_by_name("libx264");
+  if (!encoder->video_avc) {
     printf("Cannot find encoder %d\n", params->codec_id);
     return -1;
   }
 
-  encoder->avcc = avcodec_alloc_context3(encoder->avc);
-  if (!encoder->avcc) {
+  encoder->video_avcc = avcodec_alloc_context3(encoder->video_avc);
+  if (!encoder->video_avcc) {
     printf("Could not allocate encoder context\n");
     return -1;
   }
 
-  encoder->avcc->bit_rate = 1000;
-  encoder->avcc->width = DST_WIDTH;
-  encoder->avcc->height = DST_HEIGHT;
-  encoder->avcc->time_base = av_inv_q(decoder->avcc->framerate);
-  encoder->avcc->framerate = decoder->avcc->framerate;
-  encoder->avs->time_base = encoder->avcc->time_base;
-  encoder->avcc->pix_fmt = AV_PIX_FMT_YUV420P;
+  encoder->video_avcc->bit_rate = 1000;
+  encoder->video_avcc->width = DST_WIDTH;
+  encoder->video_avcc->height = DST_HEIGHT;
+  encoder->video_avcc->time_base = av_inv_q(decoder->video_avcc->framerate);
+  encoder->video_avcc->framerate = decoder->video_avcc->framerate;
+  encoder->video_avs->time_base = encoder->video_avcc->time_base;
+  encoder->video_avcc->pix_fmt = AV_PIX_FMT_YUV420P;
 
   if (encoder->avfc->oformat->flags & AVFMT_GLOBALHEADER)
-                encoder->avcc->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+    encoder->video_avcc->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
 
-  if (avcodec_open2(encoder->avcc, encoder->avc, NULL) < 0) {
+  if (avcodec_open2(encoder->video_avcc, encoder->video_avc, NULL) < 0) {
     printf("Could not open encoder\n");
     return -1;
   }
 
-  encoder->avs = avformat_new_stream(encoder->avfc, NULL);
+  encoder->video_avs = avformat_new_stream(encoder->avfc, NULL);
   if (avio_open(&encoder->avfc->pb, "output.mp4", AVIO_FLAG_WRITE) < 0) {
     printf("Could not create output video stream\n");
     return -1;
   }
 
   // Copy the codec parameters from the codec context to the stream's codecpar
-  if (avcodec_parameters_from_context(encoder->avs->codecpar, encoder->avcc) <
-      0) {
+  if (avcodec_parameters_from_context(encoder->video_avs->codecpar,
+                                      encoder->video_avcc) < 0) {
     printf("Failed to copy codec parameters to output stream\n");
     return -1;
   }
@@ -161,10 +160,11 @@ int init(const char *filename) {
   }
 
   // setup sws
-  sws_ctx = sws_getContext(decoder->avcc->width, decoder->avcc->height,
-                           decoder->avcc->pix_fmt, encoder->avcc->width,
-                           encoder->avcc->height, AV_PIX_FMT_YUV420P,
-                           SWS_BILINEAR, NULL, NULL, NULL);
+  sws_ctx =
+      sws_getContext(decoder->video_avcc->width, decoder->video_avcc->height,
+                     decoder->video_avcc->pix_fmt, encoder->video_avcc->width,
+                     encoder->video_avcc->height, AV_PIX_FMT_YUV420P,
+                     SWS_BILINEAR, NULL, NULL, NULL);
   if (!sws_ctx) {
     printf("Could not get sws context\n");
     return -1;
@@ -173,17 +173,74 @@ int init(const char *filename) {
   return 0;
 }
 
+void encode_video(MediaContext* encoder, AVRational dec_time_base, AVPacket* enc_pkt, AVFrame* enc_frame) {
+  if (avcodec_send_frame(encoder->video_avcc, enc_frame) < 0) {
+      printf("Could not send frame to encoder\n");
+      return;
+    }
+
+    int rp_res = 0;
+    while (rp_res >= 0) {
+      rp_res = avcodec_receive_packet(encoder->video_avcc, enc_pkt);
+      if (rp_res == AVERROR(EAGAIN) || rp_res == AVERROR_EOF) {
+        break;
+      } else if (rp_res < 0) {
+        printf("Encoder could not receive encoded packet\n");
+        break;
+      }
+
+      enc_pkt->stream_index = 0;
+      av_packet_rescale_ts(enc_pkt, dec_time_base,
+                           encoder->video_avs->time_base); // important
+
+      if (av_write_frame(encoder->avfc, enc_pkt) < 0) {
+        printf("Could not write frame to encoded packet\n");
+        return;
+      }
+
+      av_packet_unref(enc_pkt);
+    }
+}
+
+void transcode_video(MediaContext* decoder, MediaContext* encoder, AVPacket* dec_pkt, AVFrame* dec_frame, AVPacket* enc_pkt, AVFrame* enc_frame) {
+  if (avcodec_send_packet(decoder->video_avcc, dec_pkt) < 0) {
+    printf("Could not send packet to decoder, cleaning up...\n");
+    return;
+  }
+
+  int rf_res = 0;
+  while (rf_res >= 0) {
+    rf_res = avcodec_receive_frame(decoder->video_avcc, dec_frame);
+    if (rf_res == AVERROR(EAGAIN) || rf_res == AVERROR_EOF) {
+      break;
+    } else if (rf_res < 0) {
+      printf("Decoder could not receive frame\n");
+      break;
+    }
+
+    sws_scale(sws_ctx, (const uint8_t *const *)dec_frame->data,
+              dec_frame->linesize, 0, dec_frame->height, enc_frame->data,
+              enc_frame->linesize);
+
+    enc_frame->pts = dec_frame->pts;
+    encode_video(encoder, decoder->video_avs->time_base, enc_pkt, enc_frame);
+  }
+
+  av_packet_unref(dec_pkt);
+}
+
 int _transcode(std::string filename) {
   if (!is_callback_set)
     av_log_set_callback(custom_log_callback);
 
-  if (init(filename.c_str()) < 0) return 1;
+  if (init(filename.c_str()) < 0)
+    return 1;
 
   AVFrame *dec_frame = av_frame_alloc();
   AVFrame *enc_frame = av_frame_alloc();
   enc_frame->format = AV_PIX_FMT_YUV420P;
-  enc_frame->width = encoder->avcc->width;
-  enc_frame->height = encoder->avcc->height;
+  enc_frame->width = encoder->video_avcc->width;
+  enc_frame->height = encoder->video_avcc->height;
   av_frame_get_buffer(enc_frame, 32);
 
   AVPacket *dec_pkt = av_packet_alloc();
@@ -191,79 +248,12 @@ int _transcode(std::string filename) {
 
   while (av_read_frame(decoder->avfc, dec_pkt) >= 0) {
     if (dec_pkt->stream_index == vid_idx) {
-      if (avcodec_send_packet(decoder->avcc, dec_pkt) < 0) {
-        printf("Could not send packet to decoder, cleaning up...\n");
-        break;
-      }
-
-      int rf_res = 0;
-      while (rf_res >= 0) {
-        rf_res = avcodec_receive_frame(decoder->avcc, dec_frame);
-        if (rf_res == AVERROR(EAGAIN) || rf_res == AVERROR_EOF) {
-          break;
-        } else if (rf_res < 0) {
-          printf("Decoder could not receive frame\n");
-          break;
-        }
-
-        sws_scale(sws_ctx, (const uint8_t *const *)dec_frame->data,
-                  dec_frame->linesize, 0, dec_frame->height, enc_frame->data,
-                  enc_frame->linesize);
-
-        enc_frame->pts = dec_frame->pts;
-
-        if (avcodec_send_frame(encoder->avcc, enc_frame) < 0) {
-          printf("Could not send frame to encoder\n");
-          break;
-        }
-
-        int rp_res = 0;
-        while (rp_res >= 0) {
-          rp_res = avcodec_receive_packet(encoder->avcc, enc_pkt);
-          if (rp_res == AVERROR(EAGAIN) || rp_res == AVERROR_EOF) {
-            break;
-          } else if (rp_res < 0) {
-            printf("Encoder could not receive encoded packet\n");
-            break;
-          }
-
-          enc_pkt->stream_index = 0;
-          av_packet_rescale_ts(enc_pkt, decoder->avs->time_base, encoder->avs->time_base); // important
-
-          if (av_write_frame(encoder->avfc, enc_pkt) < 0) {
-            printf("Could not write frame to encoded packet\n");
-            return -1;
-          }
-
-          av_packet_unref(enc_pkt);
-        }
-      }
-      av_packet_unref(dec_pkt);
+      transcode_video(decoder, encoder, dec_pkt, dec_frame, enc_pkt, enc_frame);
     }
   }
 
-  avcodec_send_packet(decoder->avcc, NULL);
-  while (avcodec_receive_frame(decoder->avcc, dec_frame) >= 0) {
-    sws_scale(sws_ctx, (const uint8_t *const *)dec_frame->data,
-              dec_frame->linesize, 0, dec_frame->height, enc_frame->data,
-              enc_frame->linesize);
-    enc_frame->pts = dec_frame->pts;
-    avcodec_send_frame(encoder->avcc, enc_frame);
-    while (avcodec_receive_packet(encoder->avcc, enc_pkt) >= 0) {
-      enc_pkt->stream_index = encoder->avs->index;
-      av_packet_rescale_ts(enc_pkt, decoder->avs->time_base, encoder->avs->time_base);
-      av_write_frame(encoder->avfc, enc_pkt);
-      av_packet_unref(enc_pkt);
-    }
-  }
-
-  avcodec_send_frame(encoder->avcc, NULL);
-  while (avcodec_receive_packet(encoder->avcc, enc_pkt) >= 0) {
-    enc_pkt->stream_index = encoder->avs->index;
-    av_packet_rescale_ts(enc_pkt, decoder->avs->time_base, encoder->avs->time_base);
-    av_write_frame(encoder->avfc, enc_pkt);
-    av_packet_unref(enc_pkt);
-  }
+  transcode_video(decoder, encoder, NULL, dec_frame, enc_pkt, enc_frame); // flush video decoder
+  encode_video(encoder, decoder->video_avs->time_base, NULL, enc_frame);  // flush video encoder
 
   av_write_trailer(encoder->avfc);
 
@@ -274,8 +264,8 @@ int _transcode(std::string filename) {
   av_packet_free(&enc_pkt);
   av_frame_free(&dec_frame);
   av_frame_free(&enc_frame);
-  avcodec_free_context(&decoder->avcc);
-  avcodec_free_context(&encoder->avcc);
+  avcodec_free_context(&decoder->video_avcc);
+  avcodec_free_context(&encoder->video_avcc);
   sws_freeContext(sws_ctx);
   free(encoder);
   free(decoder);
